eb0fc6911da7ec7749e5c90aa61b7c43ce5025a3
gitlab@srinivas.gs <code@srinivas.gs>
2019-12-16 13:47:18 -0500

    wrote part of thoth.cleanup

--- +thoth/cleanup.m
+++ +thoth/cleanup.m
@@ -1,2 +1,42 @@
+% deletes distance files that do not correspond to anything
+% in isi_data
+
 function cleanup()
 
+isi_data_dir = getpref('thoth','isi_data_dir');
+isi_distance_dir = getpref('thoth','isi_distance_dir');
+
+
+assert(~isempty(isi_data_dir),'isi_data_dir not set')
+assert(~isempty(isi_distance_dir),'isi_distance_dir not set')
+
+existing_distance_files = dir([isi_distance_dir filesep '*.mat']);
+existing_distance_files = {existing_distance_files.name};
+existing_distance_hashes = existing_distance_files;
+
+
+% we only care about the hash
+D_filenames = thoth.generateFilenames(1);
+for i = 1:length(D_filenames)
+	D_filenames{i} = D_filenames{i}(end-9:end-4);
+end
+
+for i = 1:length(existing_distance_hashes)
+	existing_distance_hashes{i} = existing_distance_hashes{i}(end-9:end-4);
+end
+
+purge_these = false(length(existing_distance_hashes),1);
+
+for i = 1:length(purge_these)
+
+	if isempty(find(strcmp(existing_distance_hashes{i}, D_filenames)))
+		purge_these(i) = true;
+	end
+
+
+end
+
+if any(purge_these)
+	disp('not coded, need to purge old files...')
+	keyboard
+end
--- +thoth/measure.m
+++ +thoth/measure.m
@@ -10,8 +10,6 @@ if nargin == 0
 	Variant = 4;
 end
 
-
-
 isi_data_dir = getpref('thoth','isi_data_dir');
 isi_distance_dir = getpref('thoth','isi_distance_dir');
 
@@ -19,69 +17,8 @@ isi_distance_dir = getpref('thoth','isi_distance_dir');
 assert(~isempty(isi_data_dir),'isi_data_dir not set')
 assert(~isempty(isi_distance_dir),'isi_distance_dir not set')
 
-allexp = dir(isi_data_dir);
 
-
-% remove junk
-rm_this = false(length(allexp),1);
-for i = 1:length(rm_this)
-	if strcmp(allexp(i).name(1),'.')
-		rm_this(i) = true;
-	end
-end
-allexp(rm_this) = [];
-
-
-% look in every experimental folder and determine all ISI types
-all_types = {};
-for i = 1:length(allexp)
-	these_types = dir([allexp(i).folder filesep allexp(i).name]);
-	all_types = unique([all_types {these_types.name}]);
-end
-
-% remove junk
-rm_this = cellfun(@(x) strcmp(x(1),'.'),all_types);
-all_types(rm_this) = [];
-
-% make a list of all things to measure and what to use
-D_filenames = cell(length(allexp)*length(allexp)*length(all_types),1);
-use_isisA = cell(length(allexp)*length(allexp)*length(all_types),1);
-use_isisB = cell(length(allexp)*length(allexp)*length(all_types),1);
-use_type = cell(length(allexp)*length(allexp)*length(all_types),1);
-
-idx = 1;
-for i = 1:length(allexp)
-	for j = 1:length(allexp)
-		for k = 1:length(all_types)
-
-			isi_file1 = dir([allexp(i).folder filesep allexp(i).name filesep all_types{k} filesep '*.mat']);
-			isi_file2 = dir([allexp(j).folder filesep allexp(j).name filesep all_types{k} filesep '*.mat']);
-
-
-			if isempty(isi_file1)
-				continue
-			end
-
-			if isempty(isi_file2)
-				continue
-			end
-
-			% this hash comes from the hashes of the two constituent ISI files
-			H = hashlib.md5hash([isi_file1.name isi_file2.name]);
-			H = H(1:6);
-
-			D_filenames{idx} = [allexp(i).name '_' allexp(j).name '_' all_types{k} '_' mat2str(Variant) '_' H '.mat'];
-
-			use_isisA{idx} = allexp(i).name;
-			use_isisB{idx} = allexp(j).name;
-			use_type{idx} = all_types{k};
-
-
-			idx = idx + 1;
-		end
-	end
-end
-
+[D_filenames, use_isisA, use_isisB, use_type, idx] = thoth.generateFilenames(Variant);
 
 
 
