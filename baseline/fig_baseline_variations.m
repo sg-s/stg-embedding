076a3e357f64c6d8db65be7a520fadf727e255f5
Srinivas Gorur-Shandilya <code@srinivas.gs>
2020-07-20 13:21:30 -0400

    finished baseline variation fig

--- baseline/fig_baseline_variations.m
+++ baseline/fig_baseline_variations.m
@@ -27,40 +27,65 @@ if ~exist('metricsPD','var')
 end
 
 
+
+colors = display.colorscheme(alldata.idx);
+
+
+
+
+
 figure('outerposition',[300 300 1200 901],'PaperUnits','points','PaperSize',[1200 901]); hold on
 
 
 ax.experimenter = subplot(3,3,1); hold on
 [means, group_idx] = analysis.probStateGroupedBy(alldata, 'normal', 'experimenter');
+C = lines;
+group_idx = removecats(group_idx);
+experimenters = unique(group_idx);
+for i = 1:length(experimenters)
+	[S,P]=plotlib.raincloud(means(group_idx == experimenters(i)),'YOffset',i*2,'Color',C(i,:));
+	P.MarkerSize = 10;
+end
+group_idx = removecats(group_idx);
+set(gca,'YTick',[2:2:2*length(experimenters)],'YTickLabel',cellfun(@char,categories(experimenters),'UniformOutput',false))
+
+
+set(gca,'XAxisLocation','top')
+xlabel('p(normal)')
 
 
 
-violinplot(means,group_idx);
-set(gca,'View',[90 -90],'XLim',[0 7])
-set(gca,'YAxisLocation','right')
-ylabel('p(normal)')
+% does recording type correlate with normal behavior? 
 
-
-% does recording type correlate with behaviour? 
-
-subplot(3,3,4); hold on
+ax.LP_extra = subplot(3,3,4); hold on
 temp = structlib.purge(alldata,alldata.LP_channel == 'gpn');
 [means, group_idx] = analysis.probStateGroupedBy(temp, 'normal', 'LP_channel');
 
-C = [.3 .4 .7; intra_color; extra_color];
+C = [intra_color; .3 .4 .7; ; extra_color];
 
 groups = unique(group_idx);
+% reorder groups for asethetics
+groups = [groups(2); groups(1); groups(3)];
+
 for i = 1:length(groups)
-	[lines(i), shade(i)] = display.plotCDFWithError(means(group_idx==groups(i)),C(i,:));
+	[~, shade(i)] = display.plotCDFWithError(means(group_idx==groups(i)),C(i,:));
+	lines(i) = plot(NaN,NaN,'Marker','o','LineStyle','none','MarkerSize',10,'Color',C(i,:),'MarkerFaceColor',C(i,:));
 end
 for i = 1:length(shade)
 	uistack(shade(i),'bottom')
 end
 
-
 set(gca,'XTickLabel',{},'XLim',[0 1],'YLim',[0 1])
-
 legend(lines,cellfun(@char,{groups},'UniformOutput',false),'Location','northwest')
+
+[~,p]=kstest2(means(group_idx=='LP'),means(group_idx~='LP'));
+th = text(.35,.85,['\itp = ' mat2str(p,2)]);
+
+% add some lines to indicate data grouping
+ph = plot(ax.LP_extra,[.27 .33],[.75 .75],'k','HandleVisibility','off');
+ph = plot(ax.LP_extra,[.27 .33],[.92 .92],'k','HandleVisibility','off');
+ph = plot(ax.LP_extra,[.33 .33],[.75 .92],'k','HandleVisibility','off');
+ph = plot(ax.LP_extra,[.27 .27],[.67 .83],'k','HandleVisibility','off');
 
 
 
@@ -70,22 +95,31 @@ subplot(3,3,7); hold on
 groups = unique(group_idx);
 clear lines shade
 for i = 1:length(groups)
-	[lines(i), shade(i)] = display.plotCDFWithError(means(group_idx==groups(i)),C(i,:));
+	[~, shade(i)] = display.plotCDFWithError(means(group_idx==groups(i)),C(i,:));
+	lines(i) = plot(NaN,NaN,'Marker','o','LineStyle','none','MarkerSize',10,'Color',C(i,:),'MarkerFaceColor',C(i,:));
 end
 for i = 1:length(shade)
 	uistack(shade(i),'bottom')
 end
 
 legend(lines,cellfun(@char,{groups},'UniformOutput',false),'Location','northwest')
-
-
 xlabel('p(normal)')
-
 set(gca,'XLim',[0 1],'YLim',[0 1])
 
+[~,p]=kstest2(means(group_idx=='PD'),means(group_idx~='PD'));
+th = text(.1,.8,['\itp = ' mat2str(p,2)]);
 
 
-% make stacked bar charts of different states based on what's on each channel
+
+
+
+
+
+
+
+
+
+% make treemaps to show distribution of all states
 
 labels = {'LP/PD','LP/pdn','PD/lpn/lvn','pdn/lpn','pdn/lvn'};
 
@@ -118,33 +152,12 @@ for i = 1:5
 end
 
 
-% show how the burst periods change if we switch from intra to extra
-clear L lh
-ax.T_PD = subplot(4,3,9); hold on
-X = metricsPD.DominantPeriod(alldata.PD_channel == 'PD');
-X = analysis.averageBy(X,alldata.experiment_idx(alldata.PD_channel == 'PD'));
-[lines, shade] = display.plotCDFWithError(X, intra_color);
-
-
-
-Y = metricsPD.DominantPeriod(alldata.PD_channel ~= 'PD');
-Y = analysis.averageBy(Y,alldata.experiment_idx(alldata.PD_channel ~= 'PD'));
-[lines, shade] = display.plotCDFWithError(Y, extra_color);
-xlabel('T_{PD} (s)')
 
 
 
 
 
-ax.T_LP =subplot(4,3,12); hold on
-X = metricsLP.DominantPeriod(alldata.LP_channel == 'LP');
-X = analysis.averageBy(X,alldata.experiment_idx(alldata.LP_channel == 'LP'));
-[lines, shade] = display.plotCDFWithError(X, intra_color);
 
-Y = metricsLP.DominantPeriod(alldata.LP_channel ~= 'LP');
-Y = analysis.averageBy(Y,alldata.experiment_idx(alldata.LP_channel ~= 'LP'));
-[lines, shade] = display.plotCDFWithError(Y, extra_color);
-xlabel('T_{LP} (s)')
 
 
 
@@ -173,13 +186,18 @@ temp.PD_channel(temp.PD_channel ~= 'PD-intra') = 'PD-extra';
 C = [intra_color; extra_color];
 groups = categories(removecats(group_idx));
 for i = 1:length(groups)
-	[lines(i), shade(i)] = display.plotCDFWithError(means(group_idx==groups{i}),C(i,:));
+	[~, shade(i)] = display.plotCDFWithError(means(group_idx==groups{i}),C(i,:));
 end
 for i = 1:length(shade)
 	uistack(shade(i),'bottom')
 end
 xlabel('p(PD-01)')
-legend(lines,groups,'Location','southeast')
+lines(1) = plot(NaN,NaN,'Marker','o','LineStyle','none','MarkerSize',10,'Color',intra_color,'MarkerFaceColor',intra_color);
+lines(2) = plot(NaN,NaN,'Marker','o','LineStyle','none','MarkerSize',10,'Color',extra_color,'MarkerFaceColor',extra_color);
+legend(lines,{'PD-intra','PD-extra'},'Location','southeast')
+[~,p]=kstest2(means(group_idx=='PD-intra'),means(group_idx=='PD-extra'));
+text(.7,.6,['\itp = ' mat2str(p,2)]);
+ax.PD01.XLim = [0 1];
 
 
 
@@ -189,31 +207,145 @@ temp = structlib.purge(alldata,alldata.PD_channel == 'PD' );
 temp.LP_channel(temp.LP_channel == 'LP') = 'LP-intra';
 temp.LP_channel(temp.LP_channel == 'lpn') = 'LP-extra';
 temp.LP_channel(temp.LP_channel == 'lvn') = 'LP-extra';
-
 temp = structlib.purge(temp,temp.LP_channel == 'gpn' );
+
 [means, group_idx] = analysis.probStateGroupedBy(temp, 'LP-01', 'LP_channel');
+
 C = [intra_color; extra_color];
 groups = categories(removecats(group_idx));
 for i = 1:length(groups)
-	[lines(i), shade(i)] = display.plotCDFWithError(means(group_idx==groups{i}),C(i,:));
+	[~, shade(i)] = display.plotCDFWithError(means(group_idx==groups{i}),C(i,:));
 end
 for i = 1:length(shade)
 	uistack(shade(i),'bottom')
 end
-legend(lines,groups,'Location','southeast')
+lines(1) = plot(NaN,NaN,'Marker','o','LineStyle','none','MarkerSize',10,'Color',intra_color,'MarkerFaceColor',intra_color);
+lines(2) = plot(NaN,NaN,'Marker','o','LineStyle','none','MarkerSize',10,'Color',extra_color,'MarkerFaceColor',extra_color);
+legend(lines,{'LP-intra','LP-extra'},'Location','southeast')
 xlabel('p(LP-01)')
+[~,p]=kstest2(means(group_idx=='LP-intra'),means(group_idx=='LP-extra'));
+text(.7,.6,['\itp = ' mat2str(p,2)]);
+ax.LP01.XLim = [0 1];
+
+
+
+
+
+
+
+
+
+
+
+% show how the burst periods change if we switch from intra to extra
+clear L lh
+ax.T_PD = subplot(4,3,9); hold on
+X = metricsPD.DominantPeriod(alldata.PD_channel == 'PD');
+X = analysis.averageBy(X,alldata.experiment_idx(alldata.PD_channel == 'PD'));
+display.plotCDFWithError(X, intra_color);
+
+
+
+Y = metricsPD.DominantPeriod(alldata.PD_channel ~= 'PD');
+Y = analysis.averageBy(Y,alldata.experiment_idx(alldata.PD_channel ~= 'PD'));
+display.plotCDFWithError(Y, extra_color);
+xlabel('T_{PD} (s)')
+
+% fake plots for legend
+lines(1) = plot(NaN,NaN,'Marker','o','LineStyle','none','MarkerSize',10,'Color',intra_color,'MarkerFaceColor',intra_color);
+lines(2) = plot(NaN,NaN,'Marker','o','LineStyle','none','MarkerSize',10,'Color',extra_color,'MarkerFaceColor',extra_color);
+legend(lines,{'PD-intra','PD-extra'},'Location','southeast')
+[~,p]=kstest2(X,Y);
+text(2,.6,['\itp = ' mat2str(p,2)]);
+ax.T_PD.XLim = [0 3];
+
+
+
+
+clear lines
+ax.T_LP =subplot(4,3,12); hold on
+X = metricsLP.DominantPeriod(alldata.LP_channel == 'LP');
+X = analysis.averageBy(X,alldata.experiment_idx(alldata.LP_channel == 'LP'));
+display.plotCDFWithError(X, intra_color);
+
+Y = metricsLP.DominantPeriod(alldata.LP_channel ~= 'LP');
+Y = analysis.averageBy(Y,alldata.experiment_idx(alldata.LP_channel ~= 'LP'));
+display.plotCDFWithError(Y, extra_color);
+xlabel('T_{LP} (s)')
+
+% fake plots for legend
+lines(1) = plot(NaN,NaN,'Marker','o','LineStyle','none','MarkerSize',10,'Color',intra_color,'MarkerFaceColor',intra_color);
+lines(2) = plot(NaN,NaN,'Marker','o','LineStyle','none','MarkerSize',10,'Color',extra_color,'MarkerFaceColor',extra_color);
+legend(lines,{'LP-intra','LP-extra'},'Location','southeast')
+
+[~,p]=kstest2(X,Y);
+text(2,.6,['\itp = ' mat2str(p,2)]);
+
+
+
 
 figlib.pretty('FontSize',16)
-
-
-colors = display.colorscheme(alldata.idx);
+ax.experimenter.YLim = [0 14];
 ax.PD01.XColor = colors('PD-01');
-
-
 ax.LP01.XColor = colors('LP-01');
+
+
+
+
 
 % scale some plots and let them breate
 ax.LP01.Position(4) =  ax.LP01.Position(4)*.9;
 ax.PD01.Position(4) =  ax.PD01.Position(4)*.9;
 ax.T_PD.Position(4) =  ax.T_PD.Position(4)*.9;
 ax.T_LP.Position(4) =  ax.T_LP.Position(4)*.9;
+
+
+
+
+
+th.Position = [.35 .85];
+
+
+return
+
+% load dates
+load date.mat
+
+
+% convert dates into day of year
+ndays = [31 28 31 30 31 30 31 31 30 31 30 31];
+ndays = [0 cumsum(ndays)];
+
+
+T_PD_mean = NaN(length(unique_exps),1);
+T_PD_std = NaN(length(unique_exps),1);
+DayOfYear = NaN*T_PD_mean;
+for i = 1:length(unique_exps)
+
+	this_date = dates(i);
+	if isnan(this_date)
+		continue
+	end
+
+	if any(alldata.LP_channel(alldata.experiment_idx == unique_exps(i)) == 'LP')
+		continue
+	end
+
+	if any(alldata.PD_channel(alldata.experiment_idx == unique_exps(i)) == 'PD')
+		continue
+	end
+
+	this_date = mat2str(this_date);
+	DayOfYear(i) = ndays(str2double(this_date(5:6)))+str2double(this_date(7:end));
+
+
+	this_T = metricsPD.DominantPeriod(alldata.experiment_idx == unique_exps(i) & metricsPD.ACF_values > .9);
+	T_PD_mean(i) = nanmean(this_T);
+	T_PD_std(i) = nanstd(this_T);
+
+end
+
+
+figure('outerposition',[300 300 1200 600],'PaperUnits','points','PaperSize',[1200 600]); hold on
+errorbar(DayOfYear,T_PD_mean,T_PD_std,'o')
+
