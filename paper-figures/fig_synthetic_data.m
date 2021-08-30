cb6704a3004148be8fc34b3696fa66050c52e41a
Srinivas Gorur-Shandilya <code@srinivas.gs>
2021-08-30 15:14:36 -0400

    cleaned up fig_synthetic

--- paper-figures/fig_synthetic_data.m
+++ paper-figures/fig_synthetic_data.m
@@ -2,42 +2,50 @@
 
 N = 10000-1;
 
-data = synthetic.makeData;
 
-data = rmfield(data,'burst_period');
-data = rmfield(data,'firing_rate');
+if ~exist('data','var')
+	data = synthetic.makeData;
 
-data.mask = ones(N,1);
-data.PD = data.PD';
-data.LP = data.LP';
+	data = rmfield(data,'burst_period');
+	data = rmfield(data,'firing_rate');
 
-
-data.PD(end,:) = [];
-data.LP(end,:) = [];
-data.experiment_idx(end) = [];
-
-data = embedding.DataStore(data);
+	data.mask = ones(N,1);
+	data.PD = data.PD';
+	data.LP = data.LP';
 
 
+	data.PD(end,:) = [];
+	data.LP(end,:) = [];
+	data.experiment_idx(end) = [];
 
-% sort spikes
-data.PD = sort(data.PD,2);
-data.LP = sort(data.LP,2);
+	% sort spikes
+	data.PD = sort(data.PD,2);
+	data.LP = sort(data.LP,2);
 
 
-data = computeISIs(data);
+	data.LP_channel = repmat(categorical("LP"),N,1);
+	data.PD_channel = repmat(categorical("PD"),N,1);
 
-clear LP_PD PD_LP
-SynData = data.spikes2percentiles;
 
-SynR = embedding.tsne_data(data,PD_LP, LP_PD, VectorizedData);
+	data = embedding.DataStore(data);
+
+	data = computeISIs(data);
+
+	clear LP_PD PD_LP
+	SynData = data.spikes2percentiles;
+
+end
+
+SynR = embedding.tsne_data(data,PD_LP, LP_PD, SynData);
 
 cats = unique(data.experiment_idx);
 
 
 figure('outerposition',[300 300 1200 600],'PaperUnits','points','PaperSize',[1200 600]); hold on
-
+colors = colormaps.dcol(length(cats));
 for i = 1:length(cats)
 	this = data.experiment_idx == cats(i);
-	plot(SynR(this,1),SynR(this,2),'.')
+	plot(SynR(this,1),SynR(this,2),'.','Color',colors(i,:))
+	l(i) = plot(NaN,NaN,'.','MarkerSize',40,'Color',colors(i,:));
 end
+legend(l,cats)
